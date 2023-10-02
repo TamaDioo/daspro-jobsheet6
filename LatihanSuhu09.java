37e1eb7c931cd9b759d7455fe7403d51d6833469
Tama Dioo <tamadio91@gmail.com>
2023-10-02 17:10:03 +0700

    push dan commit LatihanSuhu09.java

--- LatihanSuhu09.java
+++ LatihanSuhu09.java
@@ -1,3 +1,25 @@
+import java.util.Scanner;
 public class LatihanSuhu09 {
-    
+    public static void main(String[] args) {
+        Scanner input09 = new Scanner(System.in);
+
+        int suhu;
+        char hujan;
+
+        System.out.print("Masukan Suhu : ");
+        suhu = input09.nextInt();
+        System.out.print("Apakah Hujan : ( ya atau tidak) ");
+        hujan = input09.next().charAt(0);
+        if(suhu > 27) {
+            System.out.println("Memakai Dress"); {
+                if(hujan == 'y'|| hujan == 'Y') {
+                    System.out.println("Membawa Payung");
+                } else {
+                    System.out.println("Memakai Sunscreen");
+                }
+            }
+        } else {
+            System.out.println("Memakai Celana Panjang");
+        }
+    }
 }

3cd78537115bab8cca542576b28369b09588894b
Tama Dioo <tamadio91@gmail.com>
2023-10-02 13:51:17 +0700

    Pemilihan 2 percobaan 2

--- Pemilihan2Percobaan109.java
+++ Pemilihan2Percobaan109.java
@@ -5,10 +5,12 @@ public class Pemilihan2Percobaan109 {
         System.out.print("Masukkan Tahun: ");
         int tahun = input09.nextInt();
             if  ((tahun % 4) == 0 ) {
-                if ((tahun % 100)!= 0){
+                if ((tahun % 100) == 0){
+                    if ((tahun %400 ) == 0) {
                     System.out.println("Tahun Kabisat");       
             }else 
             System.out.println("Bukan Tahun Kabisat");
+            }
         }
     }
 }
